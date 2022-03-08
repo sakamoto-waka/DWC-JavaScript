51d3522b6e69cfc765389a5908d800d94b20cf9c
sakamoto-waka <wakanmhl@gmail.com>
2022-03-08 12:07:35 +0900

    [finish] j-6

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -30,15 +30,31 @@ $(function(){
   // 非表示　→　一瞬だけ表示
   // $('.box1').hide();
 
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': 'blue',
-      'width': '200px',
-      'height': '100px',
-    }).$('.box1').slideUp();
+  // $('.box1').slideDown(function(){
+  //   $('.box1').css({
+  //     'background-color': 'blue',
+  //     'width': '200px',
+  //     'height': '100px',
+  //   }).$('.box1').slideUp();
+  // });
+
+  // $('.box1').mouseover(function() {
+  //   $('.box1').css({'background-color': '#0000FF'});
+  // });
+  // $('.box1').mouseout(function() {
+  //   $('.box1').css({'background-color': '#FF0000'});
+  // });
+
+  $('.box1').mouseover(function() {
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function() {
+    $('.box1').removeClass('box1-ext');
   });
 
-
+  
+  
+  
 
 
 });
