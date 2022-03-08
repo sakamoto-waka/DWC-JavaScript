664e3f1e24fe721209e14adb8d45deff12521f41
sakamoto-waka <wakanmhl@gmail.com>
2022-03-08 10:10:12 +0900

    [finish] j-4

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,14 @@
+// // documentがhtml(表示画面)を取得したあとにjQueryを実行させる関数、
+// htmlとの紐付けを行っている。
+// $(document).ready(function(){
+//   $(セレクタ).メソッド(パラメータ);
+//     $(cssのやつ).何を行うか(その内容を書き換える、指定しない場合メソッドを取得)
+//   どこに対してなにを行うか
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+//　　「bodyタグ内のHTMLを<h1>Hello jQuery!!</h!>で書き換えなさい」の意味
+// });
+
+// 省略形
+$(function(){
+  // jQueryプログラム内容
+});
