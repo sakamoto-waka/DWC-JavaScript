7f53eeb4fb7ff8b2c67783a9b50ec1f1cbcc4477
sakamoto-waka <wakanmhl@gmail.com>
2022-03-08 22:52:00 +0900

    [finish] j-11

---
+++ Tabbar-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#tab-contents .tab[id != "tab1"]').hide();
+
+  $('#tab-menu a').on('click', function(e) {
+    $('#tab-contents .tab').hide();
+    $('#tab-menu .active').removeClass('active');
+    $(this).addClass('active');
+    $($(this).attr('href')).show();
+    event.preventDefault();
+  });
+
+});
